8aa9d15a859f8c0d71fc5618fbb575160e622969
BladimirSierra <sierrabladimir218@gmail.com>
2023-02-27 18:02:35 -0600

    Agregar archivos de proyecto.

---
+++ EJERCICIO12JBSM/EJERCICIO12JBSM/Models/Personas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EJERCICIO12JBSM.Models
+{
+    public class Personas
+    {
+        public String name { set; get; }
+        public String apellidos { set; get; }
+        public int edad { set; get; }
+        public String correo { set; get; }
+    }
+}
